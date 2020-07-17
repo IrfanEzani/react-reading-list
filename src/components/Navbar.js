5004aead00418871c0913d17939e016c1f825eac
IrfanEzani <irfanezani@gmail.com>
2020-07-17 23:44:12 +0800

    consuming, creating multiple context

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,38 @@
-import React, { Component } from 'react';
-import { ThemeContext } from '../contexts/ThemeContext';
+import React, { Component } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
+import { AuthContext } from "../contexts/AuthContext";
 
 class Navbar extends Component {
   // static contextType = ThemeContext;
   render() {
-    return ( 
-      <ThemeContext.Consumer>{(context) => {
-        const { isLightTheme, light, dark } = context;
-        const theme = isLightTheme ? light : dark;
-        return (
-          <nav style={{ background: theme.ui, color: theme.syntax }}>
-            <h1>Context App</h1>
-            <ul>
-              <li>Home</li>
-              <li>About</li>
-              <li>Contact</li>
-            </ul>
-          </nav>
-        )
-      }}</ThemeContext.Consumer>
+    return (
+      <AuthContext.Consumer>
+        {(authContext) => (
+          <ThemeContext.Consumer>
+          {(themeContext) => {
+            const {isAuthenticated, toggleAuth} = authContext;
+            const { isLightTheme, light, dark } = themeContext;
+            const theme = isLightTheme ? light : dark;
+            return (
+              <nav style={{ background: theme.ui, color: theme.syntax }}>
+                <h1>Context App</h1>
+                <div>
+                  <button onClick={toggleAuth}>Log in/out</button>
+                  {isAuthenticated ? 'You are logged in' : 'You are logged out'}
+                </div>
+                <ul>
+                  <li>Home</li>
+                  <li>About</li>
+                  <li>Contact</li>
+                </ul>
+              </nav>
+            );
+          }}
+        </ThemeContext.Consumer>
+        )}
+      </AuthContext.Consumer>
     );
   }
 }
- 
+
 export default Navbar;
